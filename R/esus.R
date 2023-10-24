885cf8505180b8c3649bbffe66b405825c47b4d4
eduardompeixoto <46012106+eduardompeixoto@users.noreply.github.com>
2023-10-24 18:04:58 -0300

    Update esus.R

--- R/esus.R
+++ R/esus.R
@@ -1,145 +1,30 @@
 esus<-function(){
 
-devtools::install_github("https://github.com/covid19br/nowcaster")
-install.packages("sn")
-require(sn)
-require(readr)  
-srag <- read.csv2("http://sistemas.saude.rj.gov.br/tabnetbd/dash/now_casting_sivep.csv",header = F)
-colnames<-c("classi_fin", "dt_digita", "dt_notific", "dt_sin_pri","dt_intern","municipio_res")
-colnames(srag)<-colnames
+  library(dplyr)
+  library(readr)
 
-require(nowcaster)
+  base2 <- read_delim("http://sistemas.saude.rj.gov.br/tabnetbd/dash/resumo_internacoes_sivep2.csv",
+                    delim = ";", escape_double = FALSE, locale = locale(encoding = "WINDOWS-1252"),trim_ws = TRUE)
 
-a<-srag
+colnames(base2)<-c('Classificação final','tp_idade','Idade','Idade detalhada','Ano epidemiológico de internação','Semana epidemiológica de internação','Número de internações','Número de óbitos por SRAG')
 
-
-
-a$reference_date=as.Date(a$dt_intern,"%Y/%m/%d")
-
-a$report_date=as.Date(a$dt_digita, "%Y/%m/%d")
-
-a<-subset(a,is.na(a$dt_digita)==F)
-a<-subset(a,is.na(a$dt_sin_pri)==F)
-
-srag_geral<-a
-srag_influzenza<-subset(a,a$classi_fin==1)
-srag_covid<-subset(a,a$classi_fin==5)
-srag_outro_virus<-subset(a,a$classi_fin==2)
+base2<-base2 %>% mutate(   Faixa = case_when(Idade <= 1 ~ "Até 1",
+                                             Idade > 1 & Idade <= 4 ~ "1-4",
+                                             Idade >= 5 & Idade <= 9 ~ "5-9",
+                                             Idade >= 10 & Idade <= 19 ~ "10-19",
+                                             Idade >= 20 & Idade <= 29 ~ "20-29",
+                                             Idade >= 30 & Idade <= 39 ~ "30-39",
+                                             Idade >= 40 & Idade <= 49 ~ "40-49",
+                                             Idade >= 50 & Idade <= 59 ~ "50-59",
+                                             Idade >= 60 & Idade <= 69 ~ "60-69",
+                                             Idade >= 70 & Idade <= 79 ~ "70-79",
+                                             Idade >= 80 ~ "80+"))
 
 
+base2$faixa<-ordered(base2$Faixa,c("Até 1","1-4","5-9","10-19","20-29","30-39","40-49","50-59","60-69","70-79","80+"))
+base2$`Semana epidemiológica de internação`<-as.numeric(base2$`Semana epidemiológica de internação`)
 
-nowcasting1 <-
-  nowcasting_inla(
-    dataset = srag_geral,
-    date_onset = "reference_date",
-    date_report = "report_date",
-    data.by.week = T  )
-
-nowcasting2<-
-  nowcasting_inla(
-    dataset = srag_covid,
-    date_onset = "reference_date",
-    date_report = "report_date",
-    data.by.week = T  )
-nowcasting3 <-
-  nowcasting_inla(
-    dataset = srag_influzenza,
-    date_onset = "reference_date",
-    date_report = "report_date",
-    data.by.week = T  )
-
-nowcasting4 <-
-  nowcasting_inla(
-    dataset = srag_outro_virus,
-    date_onset = "reference_date",
-    date_report = "report_date",
-    data.by.week = T  )
-
-
-###
-
-
-
-#####
-
-chik <- read.csv2("http://sistemas.saude.rj.gov.br/tabnetbd/dash/chik.csv",header = F)
-colnames<-c( 'dt_digita', 'dt_sin_pri', 'dt_notifica','mun_res' , 'class_fin', 'evolucao')
-colnames(chik)<-colnames
-
-
-
-chik<-subset(chik,chik$class_fin==13)
-
-a<-chik
-
-a$dt_sin_pri<- paste(substr(a$dt_sin_pri, 1, 4),
-                      
-                      substr(a$dt_sin_pri, 5, 6) ,
-                      
-                      substr(a$dt_sin_pri, 7, 8),sep="-" )
-
-a$dt_digita<- paste(substr(a$dt_digita, 1, 4),
-                    
-                    substr(a$dt_digita, 5, 6) ,
-                    
-                    substr(a$dt_digita, 7, 8),sep="-" )
-
-a$reference_date=as.Date(a$dt_sin_pri)
-a$report_date=as.Date(a$dt_digita)
-
-
-chik <-
-  nowcasting_inla(
-    dataset = a,
-    date_onset = "reference_date",
-    date_report = "report_date",
-    data.by.week = T  )
-
-
-###
-
-
-
-
-dengue <- read.csv2("http://sistemas.saude.rj.gov.br/tabnetbd/dash/dengue.csv",header = F)
-colnames<-c( 'dt_digita', 'dt_sin_pri', 'dt_notifica','muni_res', 'class_fin', 'evolucao')
-colnames(dengue)<-colnames
-
-
-dengue<-subset(dengue,dengue$class_fin!=5)
-
-
-a<-dengue
-
-a$dt_sin_pri<- paste(substr(a$dt_sin_pri, 1, 4),
-                     
-                     substr(a$dt_sin_pri, 5, 6) ,
-                     
-                     substr(a$dt_sin_pri, 7, 8),sep="-" )
-
-
-
-a$reference_date=as.Date(a$dt_sin_pri)
-a$report_date=as.Date(a$dt_digita)
-
-
-dengue <-
-  nowcasting_inla(
-    dataset = a,
-    date_onset = "reference_date",
-    date_report = "report_date",
-    data.by.week = T  )
-
-
-
-list(nowcasting1,
-  nowcasting2,
-  nowcasting3,
-  nowcasting4,
-  chik,
-  dengue)
-
-
+base2
 
 }
   
